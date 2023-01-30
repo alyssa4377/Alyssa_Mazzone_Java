5e5492056fd25af6dd174e729ea593bcc9df2509
Alyssa Mazzone <57568536+alyssa4377@users.noreply.github.com>
2023-01-29 22:07:34 -0500

    Add files via upload

---
+++ M2-Challenge-Mazzone-Alyssa/src/main/java/com/company/chatterbook/model/ChatterPost.java
@@ -0,0 +1,16 @@
+package com.company.chatterbook.model;
+
+public class ChatterPost {
+    private String text;
+
+    public ChatterPost(String text){
+        this.setText(text);
+    }
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
